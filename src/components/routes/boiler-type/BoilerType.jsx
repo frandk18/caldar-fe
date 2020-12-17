10a475311062bf8b66fed57f4b0bc380ed21a98f
Alexis Pucheta <Alexispucheta22@gmail.com>
2020-12-17 18:46:11 -0300

    setup router and folder structure

---
+++ src/components/routes/boiler-type/BoilerType.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BoilerType() {
+  return <div></div>;
+}
+
+export default BoilerType;
---
+++ src/components/routes/boiler/Boiler.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Boiler() {
+  return <div></div>;
+}
+
+export default Boiler;
