4a0cd90633c32f3d92aae1834c7702483761419d
Federico Morabito <morabitofj@gmail.com>
2020-12-19 01:24:18 -0300

    sidebar added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/layout/Header.jsx";
 import NavBar from "./components/layout/NavBar.jsx";
 import Footer from "./components/layout/Footer.jsx";
+import SideBar from "./components/layout/SideBar.jsx";
 import Technician from "./components/routes/technician/Technician.jsx";
 import Building from "./components/routes/building/Building.jsx";
 import "./App.css";
@@ -12,14 +13,21 @@ function App() {
     <div>
       <Header />
       <NavBar />
+      <div class="section-container">
+        <Router>
+          <SideBar />
+          <Switch>
+            <Route path="/" exact />
+            <Route path="/technician" component={Technician} />
+            <Route path="/building" component={Building} />
+            <Route path="/boiler" />
+            <Route path="/boiler-models" />
+            <Route path="/companies" />
+            <Route path="/reports" />
+          </Switch>
+        </Router>
+      </div>
       <Footer />
-      <Router>
-        <Switch>
-          <Route path="/" exact />
-          <Route path="/technician" component={Technician} />
-          <Route path="/building" component={Building} />
-        </Switch>
-      </Router>
     </div>
   );
 }
