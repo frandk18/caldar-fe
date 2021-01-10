b7e172feed588532cc7c2db172c3f2903d8dbd5b
marcosjmelgar <marcosjmelgar@gmail.com>
2021-01-10 18:28:06 -0300

    company refactor added

--- src/components/old/CompanyFormUI.jsx
+++ src/components/old/CompanyFormUI.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import ListItemText from "@material-ui/core/ListItemText";
+import Select from "@material-ui/core/Select";
+import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
@@ -8,21 +15,28 @@ import Fade from "@material-ui/core/Fade";
 
 function Form(props) {
   const company = props.companies.filter((company) => company._id === props.id);
+
   const [newOne, setNewOne] = useState({
+    buildings: props.editing
+      ? company[0].buildings === undefined
+        ? ""
+        : company[0].buildings
+      : "",
     CIN: props.editing ? company[0].CIN : "",
     name: props.editing ? company[0].name : "",
-    email: props.editing ? company[0].email : "",
-    phone: props.editing ? company[0].phone : "",
     address: props.editing ? company[0].address : "",
     zipcode: props.editing ? company[0].zipcode : "",
     contact: props.editing ? company[0].contact : "",
+    phone: props.editing ? company[0].phone : "",
+    email: props.editing ? company[0].email : "",
     obs: props.editing ? company[0].obs : "",
   });
   const _id = props.editing ? company[0]._id : null;
 
-  const handleChange = (evt) => {
-    const value = evt.target.value;
-    const name = evt.target.name;
+  const handleChange = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const value = e.target.value;
     setNewOne({
       ...newOne,
       [name]: value,
@@ -32,6 +46,18 @@ function Form(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addEdit(newOne, _id);
+  };
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+        overflow: "hide",
+      },
+    },
   };
 
   const useStyles = makeStyles((theme) => ({
@@ -92,6 +118,30 @@ function Form(props) {
 
   const classes = useStyles();
 
+  const buildingId = [];
+  const buildingSN = [];
+
+  newOne.buildings.forEach((value) => {
+    const building = props.buildings.filter(
+      (building) => building._id === value
+    );
+    buildingSN.push(building[0].serialNumber);
+  });
+  const [buildingSerialNumber, setBuildingSerialNumber] = useState(
+    props.editing ? buildingSN : []
+  );
+
+  const handleSelectBuildingsChange = (e) => {
+    setBuildingSerialNumber(e.target.value);
+    e.target.value.forEach((value) => {
+      const building = props.buildings.filter(
+        (building) => building.serialNumber === value
+      );
+      buildingId.push(building[0]._id);
+    });
+    newOne.buildings = buildingId;
+  };
+
   return (
     <div>
       <Modal
@@ -125,12 +175,45 @@ function Form(props) {
                     style={{ display: "flex", justifyContent: "space-around" }}
                   >
                     <div className={classes.column}>
+                      <FormControl
+                        className={classes.formControl}
+                        style={{ margin: 8 }}
+                      >
+                        <InputLabel>Buildings</InputLabel>
+                        <Select
+                          labelId="buildings"
+                          id="buildings"
+                          value={buildingSerialNumber}
+                          multiple
+                          onChange={handleSelectBuildingsChange}
+                          input={<Input />}
+                          renderValue={(selected) => selected.join(", ")}
+                          MenuProps={MenuProps}
+                        >
+                          {props.buildings.map((building) => (
+                            <MenuItem
+                              key={building._id}
+                              value={building.serialNumber}
+                            >
+                              <Checkbox
+                                checked={
+                                  buildingSerialNumber.indexOf(
+                                    building.serialNumber
+                                  ) > -1
+                                }
+                              />
+                              <ListItemText primary={building.serialNumber} />
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+
                       <TextField
                         name="CIN"
                         defaultValue={newOne.CIN}
                         onChange={handleChange}
                         label="CIN"
-                        placeholder="123546"
+                        placeholder="444565"
                         fullWidth
                         margin="normal"
                         InputLabelProps={{
@@ -145,22 +228,7 @@ function Form(props) {
                         defaultValue={newOne.name}
                         onChange={handleChange}
                         label="Name"
-                        placeholder="Google"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        variant="outlined"
-                        className={classes.input}
-                      />
-
-                      <TextField
-                        name="email"
-                        defaultValue={newOne.email}
-                        onChange={handleChange}
-                        label="Email"
-                        placeholder="marcos@gmail.com"
+                        placeholder="John Doe"
                         fullWidth
                         margin="normal"
                         InputLabelProps={{
@@ -175,7 +243,22 @@ function Form(props) {
                         defaultValue={newOne.address}
                         onChange={handleChange}
                         label="Address"
-                        placeholder="Avenida siempre viva 65"
+                        placeholder="Balcarce 54"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        variant="outlined"
+                        className={classes.input}
+                      />
+
+                      <TextField
+                        name="zipcode"
+                        defaultValue={newOne.zipcode}
+                        onChange={handleChange}
+                        label="Zip Code"
+                        placeholder="4444"
                         fullWidth
                         margin="normal"
                         InputLabelProps={{
@@ -187,6 +270,21 @@ function Form(props) {
                     </div>
 
                     <div className={classes.column}>
+                      <TextField
+                        name="contact"
+                        defaultValue={newOne.contact}
+                        onChange={handleChange}
+                        label="Contact"
+                        placeholder="John Doe"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        variant="outlined"
+                        className={classes.input}
+                      />
+
                       <TextField
                         name="phone"
                         defaultValue={newOne.phone}
@@ -204,11 +302,11 @@ function Form(props) {
                       />
 
                       <TextField
-                        name="zipcode"
-                        defaultValue={newOne.zipcode}
+                        name="email"
+                        defaultValue={newOne.email}
                         onChange={handleChange}
-                        label="Zip Code"
-                        placeholder="5555-32"
+                        label="Email"
+                        placeholder="example@something.com"
                         fullWidth
                         margin="normal"
                         InputLabelProps={{
@@ -219,22 +317,7 @@ function Form(props) {
                       />
 
                       <TextField
-                        name="contact"
-                        defaultValue={newOne.contact}
-                        onChange={handleChange}
-                        label="Contact"
-                        placeholder="John Doe"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        variant="outlined"
-                        className={classes.input}
-                      />
-
-                      <TextField
-                        name="observations"
+                        name="obs"
                         defaultValue={newOne.obs}
                         onChange={handleChange}
                         label="Observations"
